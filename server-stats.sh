682f946bd3640e25f1d0c67543e802768b281aba
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-07 02:57:03 -0600

    Complete Total Disk Usage Implement functionality Format Output

--- server-stats.sh
+++ server-stats.sh
@@ -10,16 +10,22 @@ echo -n "Total CPU Usage: "
 top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}'
 
 # Total Memory Usage
+
 echo -e "\n~~~ Total Memory Usage ~~~\n"
 top -bn1 | grep "MiB Mem" | awk '{
-                                  gsub(/,/,"");
-                                  printf "Total:\t %d MiB (100%)\n", $4
-                                  printf "Used:\t %d MiB (%.2f%)\n", $8, ($8/$4) * 100
-                                  printf "Free:\t %d MiB (%.2f%)\n", $6+$10, (($6+$10)/$4) * 100
-                                  }'
+                                    gsub(/,/,"");
+                                    printf "Total:\t%d MiB (100%)\n", $4
+                                    printf "Used:\t%d MiB (%.2f%)\n", $8, ($8/$4) * 100
+                                    printf "Free:\t%d MiB (%.2f%)\n", $6+$10, (($6+$10)/$4) * 100
+                                }'
 
-# TODO: Total Disk Usage
+# Total Disk Usage
 echo -e "\n~~ Total Disk Usage ~~~\n"
+df -h --total | grep "total" | awk '{
+                                      printf "Total:\t%s (100%)\n", $2
+                                      printf "Used:\t%s (%.2f%)\n", $3, 100-$5
+                                      printf "Free:\t%s (%.2f%)\n", $4, $5
+                                    }'
 
 # TODO: Top 5 Processes by CPU Usage
 echo -e "\n~~ Top 5 Processes (CPU Usage) ~~~\n"
