da33e8ec924bc115ed3e524c89584087ae602c0d
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-08 03:59:12 -0600

    Add TODOs

---
+++ log-archive-tool/log-archive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello"
+
+# TODO: Get input from the user
+
+# TODO: Compress logs in directory
+
+# TODO: Store in new directory
+
+# TODO: Name archive file "logs_archive_YYYYMMDD_HHMMSS.tar.gz"
