d030db88cf935219d88759c3c3b1a828de487fff
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-08 06:40:45 -0600

    Log Analyser Start Add nginx-log-analyser directory Create log-analyser.sh file Add TODOs to the file

---
+++ nginx-log-analyser/log-analyser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Tool to analyse nginx log access log files and print some
+# output
+
+# TODO: Read log files
+# TODO: Top 5 IP addresses with the most requests
+# TODO: Top 5 most requested paths
+# TODO: Top 5 response status codes
