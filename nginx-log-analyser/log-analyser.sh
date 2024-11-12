3c7ba91a3bbe706ed30df00e98b8983912f64570
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-12 06:46:23 -0600

    Add user top 5 user agent functionality

--- nginx-log-analyser/log-analyser.sh
+++ nginx-log-analyser/log-analyser.sh
@@ -14,19 +14,22 @@ LOG_FILE=$1
 
 # Top 5 IP addresses with the most requests
 
-echo -e "\n~~~ Top 5 IP addresses with the most requests:~~~\n"
+echo -e "\n~~~ Top 5 IP addresses with the most requests ~~~\n"
 cat $LOG_FILE | cut -d ' ' -f 1 | sort | uniq -c | sort -n -k1 -r | head -5 | awk '{print $2 " - " $1 " requests"}'
 
 # Top 5 most requested paths
 
-echo -e "\n~~~ Top 5 most requested paths:~~~\n"
+echo -e "\n~~~ Top 5 most requested paths ~~~\n"
 cat $LOG_FILE | awk '{print $7}' | sort | uniq -c | sort -n -k1 -r | head -5 | awk '{print $2 " - " $1 " requests"}'
 
 # Top 5 response status codes
 
-echo -e "\n~~~ Top 5 response codes:~~~\n"
+echo -e "\n~~~ Top 5 response codes ~~~\n"
 cat $LOG_FILE | awk '{print $9}' | sort | uniq -c | sort -n -k1 -r | awk '{
  for (i=1; i<=NF; i=i+2)
    if ($2 ~ /^[0-9]{3}$/)
      print $2" - "$1" requests"
    }' | head -5
+
+echo -e "\n~~~ Top 5 user agents ~~~\n"
+cat nginx-access.log | awk -F'"' '{print $6}' | sort | uniq -c | sort -n -k1 -r | head -5
