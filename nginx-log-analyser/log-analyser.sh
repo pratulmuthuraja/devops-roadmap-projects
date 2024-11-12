dae1d2b4344b01ddf088d0e05cad973c4c18fdad
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-12 03:58:06 -0600

    Add Top 5 IP with most requests feature Check for input and return error if user didn't provide a log file

--- nginx-log-analyser/log-analyser.sh
+++ nginx-log-analyser/log-analyser.sh
@@ -4,6 +4,19 @@
 # output
 
 # TODO: Read log files
+
+if [ -z "$1" ]; then
+	echo "Error: Provide a nginx access log file"
+	exit 1
+fi
+
+LOG_FILE=$1
+
 # TODO: Top 5 IP addresses with the most requests
+
+echo -e "\n~~~ Top 5 IP addresses with the most requests:~~~\n"
+cat $LOG_FILE | cut -d ' ' -f 1 | sort | uniq -c | sort -n -k1 -r | head -5 | awk '{print $2 " - " $1 " requests"}'
+
 # TODO: Top 5 most requested paths
+
 # TODO: Top 5 response status codes
