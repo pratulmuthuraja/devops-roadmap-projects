05faeb79baab24ff75a1f1f1293932f11557b39a
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-12 06:08:50 -0600

    Remove all TODOs in log-analyser.sh

--- nginx-log-analyser/log-analyser.sh
+++ nginx-log-analyser/log-analyser.sh
@@ -3,7 +3,7 @@
 # Tool to analyse nginx log access log files and print some
 # output
 
-# TODO: Read log files
+# Read log files
 
 if [ -z "$1" ]; then
 	echo "Error: Provide a nginx access log file"
@@ -12,17 +12,17 @@ fi
 
 LOG_FILE=$1
 
-# TODO: Top 5 IP addresses with the most requests
+# Top 5 IP addresses with the most requests
 
 echo -e "\n~~~ Top 5 IP addresses with the most requests:~~~\n"
 cat $LOG_FILE | cut -d ' ' -f 1 | sort | uniq -c | sort -n -k1 -r | head -5 | awk '{print $2 " - " $1 " requests"}'
 
-# TODO: Top 5 most requested paths
+# Top 5 most requested paths
 
 echo -e "\n~~~ Top 5 most requested paths:~~~\n"
 cat $LOG_FILE | awk '{print $7}' | sort | uniq -c | sort -n -k1 -r | head -5 | awk '{print $2 " - " $1 " requests"}'
 
-# TODO: Top 5 response status codes
+# Top 5 response status codes
 
 echo -e "\n~~~ Top 5 response codes:~~~\n"
 cat $LOG_FILE | awk '{print $9}' | sort | uniq -c | sort -n -k1 -r | awk '{
