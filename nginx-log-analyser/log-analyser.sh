9d74bba97d0bbeec0f1c6d8c2f4dc07f324ac760
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2024-11-12 06:07:50 -0600

    Add top 5 requested paths and top 5 response codes Add both functionality

--- nginx-log-analyser/log-analyser.sh
+++ nginx-log-analyser/log-analyser.sh
@@ -19,4 +19,14 @@ cat $LOG_FILE | cut -d ' ' -f 1 | sort | uniq -c | sort -n -k1 -r | head -5 | aw
 
 # TODO: Top 5 most requested paths
 
+echo -e "\n~~~ Top 5 most requested paths:~~~\n"
+cat $LOG_FILE | awk '{print $7}' | sort | uniq -c | sort -n -k1 -r | head -5 | awk '{print $2 " - " $1 " requests"}'
+
 # TODO: Top 5 response status codes
+
+echo -e "\n~~~ Top 5 response codes:~~~\n"
+cat $LOG_FILE | awk '{print $9}' | sort | uniq -c | sort -n -k1 -r | awk '{
+ for (i=1; i<=NF; i=i+2)
+   if ($2 ~ /^[0-9]{3}$/)
+     print $2" - "$1" requests"
+   }' | head -5
